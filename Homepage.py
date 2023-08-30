015116574a366ffbc600163c2c610a1dabe33305
Prem <prem@wiseai.tech>
2023-08-30 16:45:26 +0800

    mods

--- Homepage.py
+++ Homepage.py
@@ -17,7 +17,8 @@ st.sidebar.markdown("Select a scenario to run the calculation")
 st.sidebar.markdown("1. User uploads a file for embedding")
 st.sidebar.markdown("2. User chats with an agent for data retrieval")
 
-scenario = st.sidebar.selectbox("Navigation", ("Credit Calculation", "Scenario 1", "Scenario 2"))
+scenario = st.sidebar.selectbox(
+    "Navigation", ("Credit Calculation", "Scenario 1", "Scenario 2"))
 
 # Main content based on scenario selection
 if scenario == "Scenario 1":
@@ -29,14 +30,17 @@ elif scenario == "Scenario 2":
 else:
 
     col1, col2 = st.columns(2)
-    AZARA_MARGIN = col1.slider("Azara Margin (%)", min_value=0.0, max_value=100.0, value=20.0, step=1.0)
-    AWS_COST = col2.slider("AWS Cost ($/hour)", min_value=0.0, max_value=100.0, value=0.1, step=0.1)
+    AZARA_MARGIN = col1.slider(
+        "Azara Margin (%)", min_value=0.0, max_value=100.0, value=80.0, step=1.0)
+    AWS_COST = col2.slider("AWS Cost ($/hour)", min_value=0.0,
+                           max_value=100.0, value=0.1, step=0.1)
 
     # Inputs for GPT Token
     col1, col2, col3 = st.columns((3, 1, 2))
     with col1:
         st.header("LLM")
-        model = st.radio("Which model will you use?", ("gpt-4-8k", "gpt-4-32k", "gpt-3.5-turbo"))
+        model = st.radio("Which model will you use?",
+                         ("gpt-4-8k", "gpt-4-32k", "gpt-3.5-turbo"))
         st.session_state["MODEL"] = model
 
         # Enter text for LLM
@@ -45,7 +49,8 @@ else:
 
         if cal_button and text:
 
-            length, price = count_tokens_return_length_price(text, st.session_state["MODEL"])
+            length, price = count_tokens_return_length_price(
+                text, st.session_state["MODEL"])
             st.markdown(f'GPT model: **{st.session_state["MODEL"]}**')
             st.markdown(f"Estimated Words: **{len(text.split())}**")
             st.markdown(f"Estimated Tokens: **{length}**")
@@ -53,22 +58,24 @@ else:
 
         number_of_words = len(text.split()) if text else 0
 
-
     # Inputs for Pinecone
     with col2:
         st.header("Pinecone")
-        pinecode_pods = st.number_input("Number of Pods", min_value=1, max_value=5, value=1)
-        pinecone_retrievel_duration = st.number_input("Document Retrieval Duration (seconds)", min_value=0, max_value=1000, value=30, step=1)
+        pinecode_pods = st.number_input(
+            "Number of Pods", min_value=1, max_value=5, value=1)
+        pinecone_retrievel_duration = st.number_input(
+            "Document Retrieval Duration (seconds)", min_value=0, max_value=1000, value=30, step=1)
         pinecone_cost_per_hour = 0.096
-        cloud_provider = st.selectbox("Cloud Provider", ("aws", "gcp", "azure"))
+        cloud_provider = st.selectbox(
+            "Cloud Provider", ("aws", "gcp", "azure"))
         storage_type = 's1'
         instance_type = st.selectbox("Instance Type", ("x1", "x2", "x4", "x8"))
-
 
     # Inputs for document upload
     with col3:
         st.header("Document")
-        uploaded_file = st.file_uploader("Upload a txt file", type=["pdf", "docx", "txt"])
+        uploaded_file = st.file_uploader(
+            "Upload a txt file", type=["pdf", "docx", "txt"])
         if uploaded_file:
             doc = None
 
@@ -84,7 +91,8 @@ else:
             else:
                 raise ValueError("File type not supported!")
 
-            length, price = count_tokens_return_length_price(doc, st.session_state["MODEL"])
+            length, price = count_tokens_return_length_price(
+                doc, st.session_state["MODEL"])
             doc_button = st.button("Document Cost")
 
             if doc_button:
@@ -97,7 +105,8 @@ else:
 
     if st.button("Estimated Azara Credit for Document Embedding"):
 
-        credit = calculate_azara_credit(number_of_words, pinecode_pods, pinecone_retrievel_duration, st.session_state["MODEL"], cloud_provider, storage_type, instance_type)
+        credit = calculate_azara_credit(number_of_words, pinecode_pods, pinecone_retrievel_duration,
+                                        st.session_state["MODEL"], cloud_provider, storage_type, instance_type)
         df = pd.DataFrame(
             {
                 "Azara Credit ($)": [credit],
@@ -107,11 +116,3 @@ else:
             }
         )
         st.table(df)
-
-
-
-
-
-
-
-
--- pages/Document_QnA.py
+++ pages/Document_QnA.py
@@ -10,22 +10,49 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.callbacks import get_openai_callback
 import yaml
 
-# Load the YAML file
-with open('secrets.yaml', 'r') as f:
-    config = yaml.safe_load(f)
-
-st.title('GPT-4 + Document Embedding (Pinecone)')
+st.title('GPT-4 + Document Embedding')
 
 # Get OpenAI API key, Pinecone API key and environment, and source document input
-openai_api_key = st.text_input("OpenAI API Key")
-pinecone_api_key = st.text_input("Pinecone API Key")
+openai_api_key = st.sidebar.text_input("OpenAI API Key")
+pinecone_api_key = st.sidebar.text_input("Pinecone API Key")
+
+st.sidebar.write("Variables")
+with st.sidebar.expander("GPT-3.5-turbo"):
+    st.write("Prompt Cost per Token: $0.002")
+with st.sidebar.expander("GPT-4-8k"):
+    st.write("Prompt Cost per Token: $0.003")
+with st.sidebar.expander("GPT-4-32k"):
+    st.write("Prompt Cost per Token: $0.006")
+with st.sidebar.expander("WhatsApp"):
+    st.write("Utility:")
+    st.write("- Conversation: $0.02")
+    st.write("- Message: $0.005")
+    st.write("Service:")
+    st.write("- Conversation: $0.0022")
+    st.write("- Message: $0.005")
+
 pinecone_env = 'eu-west1-gcp'
 pinecone_index = 'newtesting'
-source_doc = st.file_uploader(
-    "Upload source document", type="pdf", label_visibility="collapsed")
-query = st.text_input("What is your question?")
 
-if st.button("Submit"):
+col1, col2 = st.columns(2)
+
+source_doc = col2.file_uploader("Upload a document", type=["pdf", "docx", "txt"])
+query = col1.text_area("What is your question?", value="Summarise this document",height=100)
+
+# Twilio Integration
+twilio_whatsapp_api_per_message = 0.005
+twilio_integration = st.checkbox("Twilio WhatsApp Integration")
+
+
+
+col1, col2 = st.columns(2)
+number_of_interactions = col1.number_input("Number of Interactions", value=1, min_value=1, max_value=20)
+average_time_per_interaction = col2.number_input("Average Time per Interaction (minutes)", value=1, min_value=1, max_value=3, step=1)
+
+if 'queries_per_hour' not in st.session_state:
+    st.session_state['queries_per_hour'] = 10
+
+if st.button("Estimate Cost"):
     if not openai_api_key or not pinecone_api_key or not pinecone_env or not pinecone_index or not source_doc or not query:
         st.warning(f"Please upload the document and provide the missing fields.")
     else:
@@ -59,6 +86,26 @@ if st.button("Submit"):
                     "Total Tokens": total_tokens,
                 })
 
-            st.success(response)
+            pinecone_cost_per_hour = 0.0960  # change this based on your config
+            aws_cost_per_hour = 1.5
+            total_cost_per_hour = (total_cost) + (pinecone_cost_per_hour) + aws_cost_per_hour
+
+            total_services_costs = pinecone_cost_per_hour + aws_cost_per_hour
+
+            if twilio_integration:
+                total_cost += twilio_whatsapp_api_per_message
+
+
+            st.header("Cost Breakdown")
+            st.write(f"Total Cost for Document Embedding: ${total_cost:.2f}")
+            st.write(f"Total Cost for Document Embedding with AWS + Pinecone: ${total_cost_per_hour + total_services_costs:.2f}")
+
+            # per minute
+            st.write(f"Total Cost for Document Embedding with AWS + Pinecone per minute: ${(total_cost_per_hour + total_services_costs)/60:.2f}")
+
+
+            # st.success(response)
         except Exception as e:
             st.error(f"An error occurred: {e}")
+
+
